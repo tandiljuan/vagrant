aa6baf208878a0dce2cf2950297a69263f816cc0
Juan Manuel Lopez <tandiljuan@gmail.com>
2014-08-11 00:43:10 -0300

    Add configuration files for a LAMP server

---
+++ chef/roles/lamp.rb
@@ -0,0 +1,19 @@
+# Name of the role. Must match the name of the file
+name 'lamp'
+
+# Override the default settings on a node.
+override_attributes(
+  'smbfs' => {
+    'install' => false,
+    'mounts' => {
+      '/dummy/mount/point' => {
+        'cifs_path' => '//dummi/cifs/path'
+      }
+    }
+  }
+)
+
+# Run list function
+run_list(
+    'recipe[cookbook-lamp]'
+)
