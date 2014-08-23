9203ef9ae5bb95e3ef3a0306c8554dc08d2d5feb
Juan Manuel Lopez <tandiljuan@gmail.com>
2014-08-23 18:30:38 -0300

    Create Berksfile and role file for `ngxmgdbpy` cookbook

---
+++ chef/roles/ngxmgdbpy.rb
@@ -0,0 +1,20 @@
+# Name of the role. Must match the name of the file
+name 'ngxmgdbpy'
+
+# Override the default settings on a node.
+override_attributes(
+  'chef_environment' => 'development',
+  'smbfs' => {
+    'install' => false,
+    'mounts' => {
+      '/dummy/mount/point' => {
+        'cifs_path' => '//dummi/cifs/path'
+      }
+    }
+  }
+)
+
+# Run list function
+run_list(
+    'recipe[cookbook-ngxmgdbpy]'
+)
