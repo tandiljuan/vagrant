4b26824f44167f6a551e173b1a2f547c356c73d0
Juan Manuel Lopez <git.nrbye@slmail.me>
2015-04-13 13:55:50 -0300

    Set version 1.0.0

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             "Vagrant Bootstrap"
+maintainer       "Juan Manuel Lopez"
+maintainer_email ""
+license          "Apache 2.0"
+description      "Vagrant workspace to bootstrap a project using Chef"
+version          "1.0.0"
+
